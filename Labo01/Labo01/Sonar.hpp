3cb4f4d9214923e9aeacde0af886b6502f930dfa
Parzival2066931 <tomy1421630@outlook.com>
2025-09-08 10:55:21 -0400

    last commit

--- Labo01/Labo01/Sonar.hpp
+++ Labo01/Labo01/Sonar.hpp
@@ -2,14 +2,9 @@
 
 #pragma once
 
-enum sonarState { TROP_PROCHE,
-                  NORMAL,
-                  TROP_LOIN };
-
 class Sonar {
   private:
     void _printDist() const;
-    sonarState _etat;
     unsigned long _currentTime;
     unsigned long _lastUpdate = 0;
     int _rate = 100;
